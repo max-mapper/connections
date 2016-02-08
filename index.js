6b929a7e673474ea508384f8ef2c21b714a06c15
Mathias Buus <mathiasbuus@gmail.com>
2016-02-08 20:47:41 +0100

    support multiple servers

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 var events = require('events')
 
-module.exports = function(server, onClose) {
+module.exports = function(servers) {
   var sockets = []
 
-  server.on('connection', function (socket) {
-    sockets.push(socket)
-    socket.on('close', function () {
-      sockets.splice(sockets.indexOf(socket), 1)
-      obj.emit('close', socket)
-      if (sockets.length === 0) obj.emit('idle')
+  if (!Array.isArray(servers)) servers = [servers]
+  servers.forEach(function (server) {
+    server.on('connection', function (socket) {
+      sockets.push(socket)
+      socket.on('close', function () {
+        sockets.splice(sockets.indexOf(socket), 1)
+        obj.emit('close', socket)
+        if (sockets.length === 0) obj.emit('idle')
+      })
     })
   })
-  
+
   var obj = new events.EventEmitter()
   obj.sockets = sockets
   obj.destroy = destroy
-  
+
   function destroy() {
     for (var i = 0; i < sockets.length; i++) {
       sockets[i].destroy()
     }
   }
-  
+
   return obj
 }
