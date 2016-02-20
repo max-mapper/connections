15093fd8285fd683b4781f54f61c9de40af78bc7
Mathias Buus <mathiasbuus@gmail.com>
2016-02-20 11:16:10 -0800

    reuse onclose closure. minor style

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 var events = require('events')
 
-module.exports = function(servers) {
+module.exports = function (servers) {
   var sockets = []
 
   if (!Array.isArray(servers)) servers = [servers]
-  servers.forEach(function (server) {
-    server.on('connection', function (socket) {
-      obj.add(socket)
-    })
-  })
+
+  for (var i = 0; i < servers.length; i++) {
+    servers[i].on('connection', obj.add)
+  }
 
   var obj = new events.EventEmitter()
   obj.sockets = sockets
   obj.destroy = destroy
   obj.add = function (socket) {
     sockets.push(socket)
-    socket.on('close', function () {
-      sockets.splice(sockets.indexOf(socket), 1)
-      obj.emit('close', socket)
-      if (sockets.length === 0) obj.emit('idle')
-    })
+    socket.on('close', onclose)
     obj.emit('connection', socket)
   }
 
-  function destroy() {
+  return obj
+
+  function onclose () {
+    sockets.splice(sockets.indexOf(this), 1)
+    obj.emit('close', this)
+    if (sockets.length === 0) obj.emit('idle')
+  }
+
+  function destroy () {
     for (var i = 0; i < sockets.length; i++) {
       sockets[i].destroy()
     }
   }
-
-  return obj
 }
